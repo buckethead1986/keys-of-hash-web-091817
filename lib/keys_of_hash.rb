e277c891726f1d5146f26aa9ca19984aca96754c
Robert Peck <rspeckpfs@gmail.com>
2017-09-08 02:43:46 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -12,3 +12,9 @@ class Hash
     output #return el array
   end
 end
+
+# class Hash
+#   def keys_of(*arg) #stolen from the solution online, just trying it out to understand it. SO much prettier than mine.
+#     map {|k, v| arg.include?(v) ? k : nil}.compact #calls map implicitly on whatever outside hash this is being used on. if the argument array includes the
+#   end                                           #currently iterating value (if that block evaluates to true), add the key to the map, if not, add nil (? k : nil) . compact removes all nils at the end.
+# end
